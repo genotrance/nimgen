2e894dc90702b92290c226dd683a198b3d092634
Joey Yakimowich-Payne <jyapayne@gmail.com>
2018-06-18 19:44:45 +0900

    Implement relative import support in header files.
    Fixes #17

--- nimgen.nim
+++ nimgen.nim
@@ -224,6 +224,8 @@ proc search(file: string): string =
       echo "File doesn't exist: " & file
       quit(1)
 
+  # Only keep relative directory
+  result = result.replace(gProjectDir & $DirSep, "")
   return result.replace(re"[\\/]", $DirSep)
 
 # ###
@@ -376,12 +378,25 @@ proc getIncls(file: string, inline=false): seq[string] =
   if inline and file in gDoneInline:
     return
 
+  let curPath = splitFile(expandFileName(file)).dir
   withFile(file):
     for f in content.findIter(re"(?m)^\s*#\s*include\s+(.*?)$"):
       var inc = f.captures[0].strip()
       if ((gQuotes and inc.contains("\"")) or (gFilter != "" and gFilter in inc)) and (not exclude(inc)):
-        result.add(
-          inc.replace(re"""[<>"]""", "").replace(re"\/[\*\/].*$", "").strip())
+        let addInc = inc.replace(re"""[<>"]""", "").replace(re"\/[\*\/].*$", "").strip()
+        try:
+          # Try searching for a local library. expandFilename will throw
+          # OSError if the file does not exist
+          let
+            finc = expandFileName(curPath / addInc)
+            fname = extractFileName(finc).search()
+
+          if fname.len() > 0:
+            # only add if the file is non-empty
+            result.add(fname.search())
+        except OSError:
+          # If it's a system library
+          result.add(addInc)
 
     result = result.deduplicate()
 
